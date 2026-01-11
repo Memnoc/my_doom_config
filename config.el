b96011917f5ab3266647979c5f1e72ef4e9f2295
Memnoc <smartdroidesign@gmail.com>
2026-01-11 16:32:58 +0000

    Initial configuration part VI

--- config.el
+++ config.el
@@ -4,6 +4,34 @@
 ;; (setq user-full-name "Your Name"
 ;;       user-mail-address "your@email.com")
 
+;;; Banner
+(defun memnoc-doom-banner ()
+  (let* ((banner '("                _       _"
+                   "               ( \\     / )"
+                   "            __  \\ \\   / /  __"
+                   "           /  \\  \\ \\ / /  /  \\"
+                   "          / /\\ \\  \\ V /  / /\\ \\"
+                   "          \\_\\ \\_\\  \\_/  /_/ /_/"
+                   ""
+                   "   ███╗   ███╗███████╗███╗   ███╗███╗   ██╗ ██████╗  ██████╗"
+                   "   ████╗ ████║██╔════╝████╗ ████║████╗  ██║██╔═══██╗██╔════╝"
+                   "   ██╔████╔██║█████╗  ██╔████╔██║██╔██╗ ██║██║   ██║██║     "
+                   "   ██║╚██╔╝██║██╔══╝  ██║╚██╔╝██║██║╚████║██║   ██║██║     "
+                   "   ██║ ╚═╝ ██║███████╗██║ ╚═╝ ██║██║ ╚███║╚██████╔╝╚██████╗"
+                   "   ╚═╝     ╚═╝╚══════╝╚═╝     ╚═╝╚═╝  ╚══╝ ╚═════╝  ╚═════╝"
+                   ""
+                   "                    < The Devil >"))
+         (longest-line (apply #'max (mapcar #'length banner))))
+    (put-text-property
+     (point)
+     (dolist (line banner (point))
+       (insert (+doom-dashboard--center
+                +doom-dashboard--width line)
+               "\n"))
+     'face 'doom-dashboard-banner)))
+
+(setq +doom-dashboard-ascii-banner-fn #'memnoc-doom-banner)
+
 ;;; Appearance
 (setq doom-font (font-spec :family "JetBrains Mono" :size 15))
 (setq doom-theme 'doom-rose-pine)
@@ -31,6 +59,12 @@
       "C-j" #'evil-window-down
       "C-k" #'evil-window-up
       "C-l" #'evil-window-right)
+(map! :leader "d x" #'+default/diagnostics)
+(map! :leader "c a" #'lsp-execute-code-action)
+(map! :leader "c a" #'eglot-code-actions)
+(map! :leader "c q" #'eglot-code-action-quickfix)
+(map! :leader "c r" #'eglot-code-action-rewrite)
+(map! :leader "c o" #'eglot-code-action-organize-imports)
 
 ;;; Neotree
 (after! neotree
