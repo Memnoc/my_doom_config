18d303190eabb9f2170e9a4e08c10a7fb9d9b3fe
Memnoc <smartdroidesign@gmail.com>
2026-01-11 15:33:11 +0000

    Initial configuration part IV

--- config.el
+++ config.el
@@ -1,98 +1,42 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
+;;; Identity
+;; (setq user-full-name "Your Name"
+;;       user-mail-address "your@email.com")
 
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
-;; (setq user-full-name "John Doe"
-;;       user-mail-address "john@doe.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-symbol-font' -- for symbols
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
+;;; Appearance
 (setq doom-font (font-spec :family "JetBrains Mono" :size 15))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-;; (setq doom-theme 'doom-one)
-;;
-;; Rose Pine
-;; URL: https://github.com/donniebreve/rose-pine-doom-emacs
 (setq doom-theme 'doom-rose-pine)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
-;; Color separation files for open terminals
+;; Window dividers (rose-pine rose accent)
 (setq window-divider-default-places t
       window-divider-default-bottom-width 1
       window-divider-default-right-width 1)
 (window-divider-mode +1)
-
-;; Set divider color (adjust to rose-pine palette or your preference)
 (custom-set-faces!
   '(window-divider :foreground "#eb6f92")
   '(window-divider-first-pixel :foreground "#eb6f92")
   '(window-divider-last-pixel :foreground "#eb6f92"))
-;;
-;;
-;; Save with CTRL+s
-(map! "C-s" #'save-buffer)
-;;
-;;
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+
+;;; Org
+(setq org-directory "~/org/")
+
+;;; Keybindings
+(map! "C-s" #'save-buffer)                ;; save
+(map! :leader "e" #'+neotree/open)        ;; file tree
+(map! :leader "9" #'+vterm/toggle)        ;; floating terminal
+(map! :leader "q q" #'save-all-and-quit)  ;; save and quit
+
+;;; Neotree
+(after! neotree
+  (map! :map neotree-mode-map
+        "a" #'neotree-create-node
+        "d" #'neotree-delete-node
+        "r" #'neotree-rename-node))
+
+;;; Functions
+(defun save-all-and-quit ()
+  (interactive)
+  (save-some-buffers t)
+  (kill-emacs))
